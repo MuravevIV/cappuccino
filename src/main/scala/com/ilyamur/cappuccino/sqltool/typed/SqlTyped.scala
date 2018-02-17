73d45b851200c7b3eb0925ddfa8929b7491b8e68
Ilya_Muravyev <ilya.yarn@gmail.com>
2018-02-18 00:01:06 +0300

    -

--- src/main/scala/com/ilyamur/cappuccino/sqltool/typed/SqlTyped.scala
+++ src/main/scala/com/ilyamur/cappuccino/sqltool/typed/SqlTyped.scala
@@ -9,7 +9,17 @@ trait SqlTyped[T] {
   private val HEAD_ROW_INDEX = 0
   private val HEAD_COLUMN_INDEX = 0
 
-  def getValue(queryRow: SqlQueryRow, column: Int): T
+  def getValue(queryRow: SqlQueryRow, idx: Int): T = {
+    val value = queryRow.getData(idx)
+    extractValue(value) match {
+      case Some(typedValue) =>
+        typedValue
+      case _ =>
+        throw report()
+    }
+  }
+
+  protected def extractValue(value: Any): Option[T]
 
   def getSingleFrom(queryRows: ArrayBuffer[SqlQueryRow]): T = {
     val queryRow = queryRows(HEAD_ROW_INDEX)
@@ -32,4 +42,6 @@ trait SqlTyped[T] {
 
   // todo
   protected def report(queryRows: ArrayBuffer[SqlQueryRow]): String = ???
+
+  protected def report(): Exception = ???
 }
--- src/main/scala/com/ilyamur/cappuccino/sqltool/typed/StringTyped.scala
+++ src/main/scala/com/ilyamur/cappuccino/sqltool/typed/StringTyped.scala
@@ -1,25 +1,11 @@
 package com.ilyamur.cappuccino.sqltool.typed
 
-import com.ilyamur.cappuccino.sqltool.component.SqlQueryRow
-
 class StringTyped extends SqlTyped[String] {
 
-  override def getValue(queryRow: SqlQueryRow, idx: Int): String = {
-    val value = queryRow.getData(idx)
-    extractValue(value) match {
-      case Some(typedValue) =>
-        typedValue
-      case _ =>
-        throw report()
-    }
-  }
-
-  private def extractValue(value: Any) = value match {
+  override protected def extractValue(value: Any): Option[String] = value match {
     case stringValue: String =>
       Some(stringValue)
     case _ =>
       None
   }
-
-  private def report(): Exception = ???
 }
