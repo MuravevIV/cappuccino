99fa782cc82da3c7037acf7761bedd680496c7e9
Ilya_Muravyev <ilya.yarn@gmail.com>
2018-02-19 00:04:28 +0300

    -

--- src/main/scala/com/ilyamur/cappuccino/sqltool/parser/SqlQueryAst.scala
+++ src/main/scala/com/ilyamur/cappuccino/sqltool/parser/SqlQueryAst.scala
@@ -1,3 +1,18 @@
 package com.ilyamur.cappuccino.sqltool.parser
 
-case class SqlQueryAst(tokens: List[SqlQueryToken])
+case class SqlQueryAst(tokens: List[SqlQueryToken]) {
+
+  def getQueryStringNormalForm: String = {
+    tokens.map {
+      case SqlQueryTextToken(text) => text
+      case SqlQueryParamToken(_, _) => "?"
+    }.mkString
+  }
+
+  def getQueryParameters: List[SqlQueryParamToken] = {
+    tokens.flatMap {
+      case SqlQueryTextToken(_) => List.empty
+      case paramToken: SqlQueryParamToken => List(paramToken)
+    }
+  }
+}
