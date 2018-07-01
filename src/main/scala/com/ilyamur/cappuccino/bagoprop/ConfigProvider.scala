54ee203cf283de72bc35ce549dd2598214ce32b5
Ilya_Muravyev <ilya.yarn@gmail.com>
2018-07-01 23:51:19 +0300

    -

--- src/main/scala/com/ilyamur/cappuccino/bagoprop/ConfigProvider.scala
+++ src/main/scala/com/ilyamur/cappuccino/bagoprop/ConfigProvider.scala
@@ -1,19 +1,10 @@
 package com.ilyamur.cappuccino.bagoprop
 
-import java.nio.charset.{Charset, StandardCharsets}
-
 import com.typesafe.config.{Config, ConfigFactory}
-import org.apache.commons.io.IOUtils
 
 class ConfigProvider {
 
   def getConfig(configPath: String): Config = {
-    val inputStream = getClass.getResourceAsStream("/" + configPath.stripPrefix("/"))
-    try {
-      val str = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
-      ConfigFactory.parseString(str)
-    } finally {
-      inputStream.close()
-    }
+    ConfigFactory.parseResourcesAnySyntax(getClass.getClassLoader, configPath)
   }
 }
